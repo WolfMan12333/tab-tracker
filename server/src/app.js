509e827303eccd71610c78accf8a10438b30d3f9
WolfMan12333 <dawidwordliczek@wp.pl>
2021-12-12 00:18:50 +0100

    finishing first part but not working exactly

--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,15 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//  res.send({
+//    message: 'hello world!'
+//  })
+// })
+
+app.get('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! your user was registered! Have fun!`
   })
 })
 
